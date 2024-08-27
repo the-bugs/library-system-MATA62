b99285a6dbe16a904fb5a68f2d5f6aa98db5c41d
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-08-26 21:59:41 -0300

    refactor: modifies inheritance and exclude unused interfaces

--- src/domain/interfaces/IReservation.java
+++
@@ -1,6 +0,0 @@
-package domain.interfaces;
-
-public interface IReservation {
-
-
-}
--- src/domain/model/user/GraduateUser.java
+++ src/domain/model/user/GraduateUser.java
@@ -3,7 +3,7 @@ package domain.model.user;
 import static domain.common.Constants.GRADUATE_STUDENT_LOAN_DAYS;
 import static domain.common.Constants.QUANTITY_BOOKS_GRADUATE_STUDENT;
 
-public class GraduateUser extends UserStudent {
+public class GraduateUser extends User {
 
     public GraduateUser(final Integer id, final String name) {
         this.setId(id);
