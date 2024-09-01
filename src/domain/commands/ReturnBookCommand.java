6e04fee746f9fb7dd252b7a9657e3c84cabe2475
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-09-01 18:29:29 -0300

    feature: implements return book logic

--- src/domain/commands/ReturnBookCommand.java
+++ src/domain/commands/ReturnBookCommand.java
@@ -6,6 +6,22 @@ public class ReturnBookCommand extends Command {
 
     @Override
     public void execute(Params params) {
-        // TODO: Implementar.
+
+        final var userId = Integer.parseInt(params.getFirstKey());
+        final var bookId = Integer.parseInt(params.getSecondKey());
+        final var user = repository.findUserById(userId);
+        final var book = repository.findBookById(bookId);
+
+        if (book != null && user != null) {
+            user.getRentedBooks().stream()
+                    .filter(bookLoan -> bookLoan.getBookCopy().getBookId().equals(bookId))
+                    .findFirst()
+                    .ifPresent(bookLoan -> {
+                        user.getRentedBooks().remove(bookLoan);
+                        logger.info("Book returned by " + user.getName());
+                    });
+        } else {
+            logger.info("User or book not found");
+        }
     }
 }
