5550eba8c2ee4f648bb2b64570359695cb503d1d
Matheus Victor <matheusvictor.salles@gmail.com>
2024-09-01 14:39:12 -0300

    feat: add constants messages

--- src/domain/common/Constants.java
+++ src/domain/common/Constants.java
@@ -5,15 +5,19 @@ public abstract class Constants {
     private Constants() {
     }
 
+    // Commands
     public static final String EXIT = "sai";
-
     public static final String BOOK_LOAN = "emp"; // Empréstimo de livro
     public static final String BOOK_RETURN = "dev"; // Devolução de livro
     public static final String BOOK_SEARCH = "liv"; // Consulta de livro
     public static final String BOOK_RESERVATION = "res"; // Reserva de livro
-
     public static final String REGISTER_OBSERVER = "obs"; // Cadastrar professor como observador
     public static final String QUANTITY_NOTIFICATION = "ntf"; // Quantidade de notificações que um professor recebeu
+
+    // Messages
+
+    public static final String EXITING = "Exiting...";
+    public static final String COMMAND_NOT_FOUND = "Command not found.";
 
     public static final Integer PROFESSOR_LOAN_DAYS = 7;
     public static final Integer GRADUATE_STUDENT_LOAN_DAYS = 5;
@@ -23,6 +27,17 @@ public abstract class Constants {
     public static final Integer QUANTITY_BOOKS_GRADUATE_STUDENT = 3;
     public static final Integer QUANTITY_BOOKS_POSTGRADUATE_STUDENT = 4;
 
-    public static final String USER_OR_BOOK_NOT_FOUND = "User or book not found.";
+    public static final String TOTAL_OF = "Total of %s: %d";
+    public static final String REPOSITORY_ADDRESS = "LibraryRepository address: %s";
 
+    public static final String USER_OR_BOOK_NOT_FOUND = "User or book not found.";
+    public static final String USER_IS_NOT_ELIGIBLE_TO_RENT_BOOK = "User %s is not eligible to rent a book. Cause: %s";
+    public static final String USER_HAS_OVERDUE_BOOKS = "User %s has overdue books";
+    public static final String USER_REACHED_MAXIMUM_BOOKS_ALLOWED =
+            "User %s is not eligible. Cause: has reached the maximum (%d) number of books allowed";
+    public static final String REMOVE_BOOK_RESERVATION_FROM = "Reservation for book with id %d was removed from user %s.";
+
+    public static final String BOOK_NOT_FOUND = "Book not found.";
+    public static final String BOOK_WAS_RENTED_BY = "%s was rented by user %s.";
+    public static final String BOOK_WAS_RESERVED_BY = "%s was reserved by user %s.";
 }
