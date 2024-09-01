dab6e3ac8b88d66a6e6fe31a0a27617b1c66d6b6
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-09-01 19:14:29 -0300

    feature: loan checker logic

--- src/service/ProfessorLoanChecker.java
+++ src/service/ProfessorLoanChecker.java
@@ -1,14 +1,28 @@
 package service;
 
 import domain.interfaces.ILoanChecker;
+import domain.model.book.BookLoan;
 import domain.model.user.User;
 
+import java.time.LocalDate;
 import java.util.Map;
+
+import static domain.common.Constants.USER_HAS_OVERDUE_BOOKS;
 
 public class ProfessorLoanChecker implements ILoanChecker {
 
     @Override
     public Map<Boolean, String> isEligibleToRentBook(User user) {
-        return Map.of();
+
+        for(BookLoan loan : user.getRentedBooks()) {
+            if(loan.getReturnDate().isBefore(LocalDate.now()) ) {
+                return Map.of(
+                        false,
+                        USER_HAS_OVERDUE_BOOKS.formatted(user.getName())
+                );
+            }
+        }
+
+        return Map.of(true, "SUCCESS");
     }
 }
